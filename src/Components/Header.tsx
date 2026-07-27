b5e55e51a5dfbcb916bc43e1788346715c941be4
Erfan Mofidi <mofidi0423@gmail.com>
2026-07-27 20:57:14 +0430

    header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,27 @@
 export const Header = () => {
   return (
-    <nav className="flex flex-row justify-between px-2 py-3 bg-linear-0 to-black from-transparent">
-      <div className="flex flex-row gap-2 text-white font-bold">
+    <nav className="flex flex-row justify-between px-2 py-6 bg-linear-0 to-black from-transparent">
+
+      <div className="flex flex-row gap-2 text-white font-medium">
         <span>N</span>
         <span>O</span>
         <span>V</span>
         <span>A</span>
       </div>
-      <div></div>
-      <div></div>
+
+      <div className="flex flex-row gap-10 font-light text-white text-sm">
+        <button className="relative cursor-pointer after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-full after:bg-green-400 after:scale-x-0 after:origin-left after:transition-transform duration-200 hover:after:scale-100">
+          SHOP
+        </button>
+        <button>TECHNOLOGY</button>
+        <button>ABOUT</button>
+        <button>JOURNAL</button>
+      </div>
+
+      <div className="text-white text-sm font-light">
+        <button>CART (0)</button>
+      </div>
+
     </nav>
   )
 }
