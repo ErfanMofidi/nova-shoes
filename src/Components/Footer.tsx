03d5ebc09ae5620a17a8f315e38a26abcadddfe1
Erfan Mofidi <mofidi0423@gmail.com>
2026-08-11 00:41:56 +0430

    completed footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="border-t border-[#71717a] py-12 px-14">
+    <div className="bg-[#060609] border-t border-[#71717a] py-12 px-14 flex flex-row justify-between items-center">
 
       <div className="flex flex-col gap-2">
 
@@ -11,11 +11,47 @@ export const Footer = () => {
           <span>A</span>
         </div>
 
+        <p className="text-[#71717a] text-sm">
+          © 2026 NOVA, All rights reserved.
+        </p>
+
       </div>
 
-      <div></div>
+      <div className="flex flex-row gap-8">
 
-      <div></div>
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          shop
+        </button>
+        
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          technology
+        </button>
+        
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          about
+        </button>
+        
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          journal
+        </button>
+
+      </div>
+      
+      <div className="flex flex-row gap-8">
+
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          instagram
+        </button>
+        
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          twitter
+        </button>
+        
+        <button className="text-[#ededed] uppercase px-4 py-2">
+          youtube
+        </button>
+
+      </div>
 
     </div>
   )
